1d63f79b299237b87bec128271c7ef2dfff204f1
Artem Plohenko <58325944+ArtemPlohenko@users.noreply.github.com>
2023-06-25 10:55:57 +0300

    creates Internet Shop wiht bug and counter

---
+++ app.js
@@ -0,0 +1,113 @@
+// Cart
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#close-cart");
+
+cartIcon.addEventListener("click", () => {
+  cart.classList.add("active");
+});
+closeCart.addEventListener("click", () => {
+  cart.classList.remove("active");
+});
+
+// Cart Working
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  const removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    let button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+
+  // Quantity Changes
+  const quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  // Add To Cart
+  const addCart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+  }
+}
+
+// Remove Items from Cart
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+// Quantity Changes
+function quantityChanged(e) {
+  let input = e.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+// Add To Cart
+function addCartClicked(e) {
+  let button = e.target;
+  let shopProducts = button.parentElement;
+  const title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  const price = shopProducts.getElementsByClassName("price")[0].innerText;
+  const productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  //   console.log(title, price, productImg);
+  addProductToCart(title, price, productImg);
+  updateTotal();
+}
+
+function addProductToCart(title, price, productImg) {
+  let cartShopBox = document.createElement("div");
+  cartShopBox.classList.add("cart-box");
+  let cartItems = document.getElementsByClassName("cart-content")[0];
+  let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+  for (let i = 0; i < cartItemsNames.length; i++) {
+    alert("You have already add this item to cart");
+    return;
+  }
+}
+
+let cartBoxContent = `
+    <img src="images/product2.jpg" alt="" class="cart-img" />
+    <div class="detail-box">
+        <div class="cart-product-title">Earbuds</div>
+        <div class="cart-price">$25</div>
+        <input type="number" value="1" class="cart-quantity" />
+    </div>
+    <!-- Remove Cart -->
+    <i class="bx bxs-trash-alt cart-remove"></i>`;
+
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
+cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
+
+// Update Total
+function updateTotal() {
+  const cartContent = document.getElementsByClassName("cart-content")[0];
+  const cartBoxes = cartContent.getElementsByClassName("cart-box");
+  let total = 0;
+
+  for (let i = 0; i < cartBoxes.length; i++) {
+    const cartBox = cartBoxes[i];
+    const priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    const quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    let quantity = quantityElement.value;
+    total = total + price * quantity;
+    total = Math.round(total * 100) / 100;
+
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  }
+}
