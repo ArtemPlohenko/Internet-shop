de5e0feb2622afe7ee6f9937e4e198d5cccb84a4
Artem Plohenko <58325944+ArtemPlohenko@users.noreply.github.com>
2023-06-25 22:17:57 +0300

    fixed addProductToCart

--- app.js
+++ app.js
@@ -87,25 +87,27 @@ function addProductToCart(title, price, productImg) {
   let cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
 
   for (let i = 0; i < cartItemsNames.length; i++) {
-    alert("You have already add this item to cart");
-    return;
+    if (cartItemsNames[i].innerText == title) {
+      alert("You have already add this item to cart");
+      return;
+    }
   }
+
+  let cartBoxContent = `
+      <img src="${productImg}" alt="" class="cart-img" />
+      <div class="detail-box">
+          <div class="cart-product-title">${title}</div>
+          <div class="cart-price">${price}</div>
+          <input type="number" value="1" class="cart-quantity" />
+      </div>
+      <!-- Remove Cart -->
+      <i class="bx bxs-trash-alt cart-remove"></i>`;
+
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.append(cartShopBox);
+  cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
+  cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
 }
-
-let cartBoxContent = `
-    <img src="${productImg}" alt="" class="cart-img" />
-    <div class="detail-box">
-        <div class="cart-product-title">${title}</div>
-        <div class="cart-price">${price}</div>
-        <input type="number" value="1" class="cart-quantity" />
-    </div>
-    <!-- Remove Cart -->
-    <i class="bx bxs-trash-alt cart-remove"></i>`;
-
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.append(cartShopBox);
-cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
-cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
 
 // Update Total
 function updateTotal() {
