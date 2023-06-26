6cc44fd7a17c9037371aed5d78a2a9f742f27f1c
Artem Plohenko <58325944+ArtemPlohenko@users.noreply.github.com>
2023-06-26 08:44:13 +0300

    added new pages

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ function addProductToCart(title, price, productImg) {
       <div class="detail-box">
           <div class="cart-product-title">${title}</div>
           <div class="cart-price">${price}</div>
-          <input type="number" value="1" class="cart-quantity" />
+          <input type="number" name="" value="1" class="cart-quantity" />
       </div>
       <!-- Remove Cart -->
       <i class="bx bxs-trash-alt cart-remove"></i>`;
